5a33c276475d2adfee484b8360e69c62cbbdc92e
Francois-Xavier Coudert <fxcoudert@gmail.com>
2021-05-20 11:05:19 +0200

    Move away from X11 requirement

--- Formula/platon.rb
+++ Formula/platon.rb
@@ -1,16 +1,18 @@
 class Platon < Formula
   desc "Multipurpose crystallographic tool"
-  homepage "http://www.cryst.chem.uu.nl/spek/platon/"
-  url "http://www.cryst.chem.uu.nl/spek/xraysoft/unix/platon.tar.gz"
+  homepage "https://www.platonsoft.nl/platon/"
+  url "https://www.platonsoft.nl/spek/xraysoft/unix/platon.tar.gz"
   version "0"
 
   depends_on "gcc" # for gfortran
-  depends_on :x11
+  depends_on "libx11"
 
   def install
     system "gunzip", "platon.f.gz", "xdrvr.c.gz"
     system "gfortran", "-o", "platon", "platon.f", "xdrvr.c",
-                       "-I#{MacOS::X11.include}", "-L#{MacOS::X11.lib}", "-lX11"
+                       "-I#{Formula["libx11"].opt_include}",
+                       "-I#{Formula["xorgproto"].opt_include}",
+                       "-L#{Formula["libx11"].opt_lib}", "-lX11"
     bin.install "platon"
 
     rm_f "platon"
